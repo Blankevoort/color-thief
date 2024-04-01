0f4a5e98fffe3a80849f6dbf5d5d2a80658d00a4
Moeen Sedaqati <moeensedaghaty86@gmail.com>
2024-04-01 05:58:42 +0430

    most basic color thief project

---
+++ color thief/src/pages/IndexPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="flex flex-center">
+    <img src="../../public/test.jpg" id="photo" />
+  </q-page>
+</template>
+
+<script>
+import ColorThief from "colorthief";
+import { onMounted } from "vue";
+
+export default {
+  setup() {
+    onMounted(() => {
+      const colorThief = new ColorThief();
+      const img = document.getElementById("photo");
+
+      if (img.complete) {
+        console.log(colorThief.getColor(img));
+      } else {
+        img.addEventListener("load", function () {
+          console.log(colorThief.getColor(img));
+        });
+      }
+    });
+
+    return {};
+  },
+};
+</script>
